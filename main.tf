0e764b52bc2787128840911a3424b1ae73715e5d
Alamshaha Shaikh <alamshahashaikh1992@gmail.com>
2025-08-24 21:54:30 +0530

    Initial22

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "google_storage_bucket" "my-bucket19" {
+resource "google_storage_bucket" "my-bucket122" {
   name                     = "bkt-demo-alam-000"
   location                 = "us-central1"
   project                  = "terraform-project-470015"
